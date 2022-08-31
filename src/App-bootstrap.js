6f1093750f8bb59815d37ae986b455a497bd196f
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-31 12:19:17 +0545

    feat: material ui applied

--- src/App-bootstrap.js
+++ src/App-bootstrap.js
@@ -1,17 +1,5 @@
-import {
-  Container,
-  Table,
-  Paper,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  TextField,
-  Button,
-  Alert,
-} from "@mui/material";
 import { useState } from "react";
-// import { Button, Table } from "react-bootstrap";
+import { Button, Form, Table, Alert, Nav, Navbar } from "react-bootstrap";
 import {
   Routes,
   Route,
@@ -23,27 +11,34 @@ import {
 import { useField } from "./hooks";
 const Home = () => (
   <div>
-    <h2>TKTL notes app</h2>{" "}
+    <h2>TKTL notes app</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
   </div>
 );
 
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <TableContainer component={Paper}>
-      <Table>
-        <TableBody>
-          {notes.map((note) => (
-            <TableRow key={note.id}>
-              <TableCell>
-                <Link to={`/notes/${note.id}`}>{note.content}</Link>
-              </TableCell>
-              <TableCell>{note.user}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => (
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>{note.user}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 const Note = ({ note }) => {
@@ -92,8 +87,7 @@ const Login = (props) => {
       {/* <form onSubmit={onSubmit}>
         <div>
           username:{" "}
-          <TextField
-            label="username"
+          <input
             type={username.type}
             value={username.value}
             onChange={username.onChangeHandler}
@@ -101,45 +95,34 @@ const Login = (props) => {
         </div>
         <div>
           password:{" "}
-          <TextField
-            label="password"
+          <input
             type={pswd.type}
             value={pswd.value}
             onChange={pswd.onChangeHandler}
           />
         </div>
-        <Button variant="contained" color="primary" type="submit">
-          login
-        </Button>
+        <button type="submit">login</button>
       </form> */}
-
-      <form onSubmit={onSubmit}>
-        <div>
-          <TextField
-            label="username"
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control
             type={username.type}
             value={username.value}
             onChange={username.onChangeHandler}
           />
-        </div>
-        <div>
-          <TextField
-            label="password"
+          <Form.Label>password:</Form.Label>
+          <Form.Control
             type={pswd.type}
             value={pswd.value}
             onChange={pswd.onChangeHandler}
           />
-        </div>
-        <div>
-          <Button variant="contained" color="primary" type="submit">
+          <Button variant="primary" type="submit">
             login
           </Button>
-        </div>
-      </form>
-      {/* <button onClick={() => resetBtn()}>reset</button> */}
-      <Button variant="contained" color="secondary" onClick={() => resetBtn()}>
-        reset
-      </Button>
+        </Form.Group>
+      </Form>
+      <button onClick={() => resetBtn()}>reset</button>
     </div>
   );
 };
@@ -171,6 +154,10 @@ function App() {
 
   const login = (user) => {
     setUser(user);
+    setMessage(`welcome ${user}`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 10000);
   };
 
   const padding = {
@@ -185,25 +172,39 @@ function App() {
 
   return (
     <div>
-      <Container>
-        {message && <Alert severity="success">{message}</Alert>}
-        <Link style={padding} to="/">
-          home
-        </Link>
-        <Link style={padding} to="/notes">
-          notes
-        </Link>
-        <Link style={padding} to="/users">
-          users
-        </Link>
-        {user ? (
-          <em>{user} logged in</em>
-        ) : (
-          <Link style={padding} to="/login">
-            login
-          </Link>
-        )}
-
+      <div className="container">
+        {message && <Alert variant="success">{message}</Alert>}
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/">
+                  home
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/notes">
+                  notes
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                <Link style={padding} to="/users">
+                  users
+                </Link>
+              </Nav.Link>
+              <Nav.Link href="#" as="span">
+                {user ? (
+                  <em style={padding}>{user} logged in</em>
+                ) : (
+                  <Link style={padding} to="/login">
+                    login
+                  </Link>
+                )}
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <Routes>
           <Route path="/notes/:id" element={<Note note={note} />} />
 
@@ -212,7 +213,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login onLogin={login} />} />
         </Routes>
-      </Container>
+      </div>
     </div>
   );
 }
